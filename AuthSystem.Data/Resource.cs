c17caad7b2a98cd2e19e2c21eb630cea1ede40e8
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-07 18:55:40 -0400

    Create IResourceManager, Resource

---
+++ AuthSystem.Data/Resource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthSystem.Data
+{
+    public struct Resource
+    {
+        public Guid Id { get; set; }
+        public string Value { get; set; }
+        
+        public Resource(Guid id, string value)
+        {
+            Id = id;
+            Value = value;
+        }
+    }
+}
