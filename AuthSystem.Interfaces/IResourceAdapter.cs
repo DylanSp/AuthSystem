3636ccb3e5e40ed36a5d0ad64a8f54ac8de2c8a5
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-07 19:53:54 -0400

    Build out IResourceManager, create IResourceAdapter

---
+++ AuthSystem.Interfaces/IResourceAdapter.cs
@@ -0,0 +1,15 @@
+﻿using AuthSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Interfaces
+{
+    public interface IResourceAdapter
+    {
+        Task<IEnumerable<Resource>> GetAllResourcesAsync();
+        Task<Resource?> GetResourceAsync(Guid resourceId);
+        Task<Guid> CreateResourceAsync(string value);
+        Task<int> UpdateResourceAsync(Resource newResource);
+    }
+}
--- AuthSystem.Interfaces/IResourceManager.cs
+++ AuthSystem.Interfaces/IResourceManager.cs
@@ -7,9 +7,9 @@ namespace AuthSystem.Interfaces
 {
     public interface IResourceManager
     {
-        Task<IEnumerable<Resource>> GetAllResourcesAsync();
-        Task<Resource?> GetResourceAsync(Guid resourceId);
-        Task<Guid> CreateResourceAsync(string value);
-        Task UpdateResourceAsync(Resource newResource); // TODO - should this return result, whether success or not?
+        Task<IEnumerable<Resource>> GetAllResourcesAsync(string username);
+        Task<Resource?> GetResourceAsync(Guid resourceId, string username);
+        Task<Guid> CreateResourceAsync(string value, string username);
+        Task UpdateResourceAsync(Resource newResource, string username); // TODO - should this return result, whether success or not?
     }
 }
