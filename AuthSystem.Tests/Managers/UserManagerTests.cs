257b25c5d3769c0817fc0820cc1ff8ca0c9c5062
Dylan Sprague <dsprague@docuphase.com>
2019-10-07 16:03:20 -0400

    Add tests for UserManager.ChangePasswordAsync()

--- AuthSystem.Tests/Managers/UserManagerTests.cs
+++ AuthSystem.Tests/Managers/UserManagerTests.cs
@@ -7,6 +7,7 @@ using AuthSystem.Interfaces;
 using AuthSystem.Managers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 
 namespace AuthSystem.Tests.Managers
 {
@@ -22,8 +23,7 @@ namespace AuthSystem.Tests.Managers
             var adapter = Substitute.For<IUserAdapter>();
             adapter.IsUserIdUniqueAsync(Arg.Any<Guid>()).Returns(true);
             adapter.IsUsernameUniqueAsync(username).Returns(false);
-            var passwordService = Substitute.For<IPasswordService>();
-            var manager = new UserManager(adapter, passwordService);
+            var manager = new UserManager(adapter, Substitute.For<IPasswordService>());
 
             // Act
             var result = await manager.CreateUserAsync(username, "");
@@ -41,14 +41,75 @@ namespace AuthSystem.Tests.Managers
             var adapter = Substitute.For<IUserAdapter>();
             adapter.IsUserIdUniqueAsync(Arg.Any<Guid>()).Returns(true);
             adapter.IsUsernameUniqueAsync(username).Returns(true);
-            var passwordService = Substitute.For<IPasswordService>();
-            var manager = new UserManager(adapter, passwordService);
+            var manager = new UserManager(adapter, Substitute.For<IPasswordService>());
 
             // Act
             var result = await manager.CreateUserAsync(username, "");
 
             // Assert
             Assert.AreEqual(CreateUserResults.UserCreated, result);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public async Task ChangePassword_WithNonexistentUser_ReturnsAppropriateError()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var adapter = Substitute.For<IUserAdapter>();
+            adapter.ReadUserAsync(userId).Returns(null as User?);
+            var manager = new UserManager(adapter, Substitute.For<IPasswordService>());
+
+            // Act
+            var result = await manager.ChangePasswordAsync(userId, "oldpass", "newpass");
+
+            // Assert
+            Assert.AreEqual(ChangePasswordResults.UserNotPresent, result);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public async Task ChangePassword_WithIncorrectOldPassword_ReturnsAppropriateError()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var adapter = Substitute.For<IUserAdapter>();
+            adapter.ReadUserAsync(userId).Returns(new User
+            {
+                Id = userId,
+                Username = "someUser",
+            });
+            var passwordService = Substitute.For<IPasswordService>();
+            passwordService.CheckIfPasswordMatchesHash(Arg.Any<string>(), Arg.Any<HashedPassword>()).Returns(false);
+            var manager = new UserManager(adapter, passwordService);
+
+            // Act
+            var result = await manager.ChangePasswordAsync(userId, "oldpass", "newpass");
+
+            // Assert
+            Assert.AreEqual(ChangePasswordResults.PasswordIncorrect, result);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public async Task ChangePassword_WithExistingUsernameAndCorrectOldPassword_ReturnsSuccess()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var adapter = Substitute.For<IUserAdapter>();
+            adapter.ReadUserAsync(userId).Returns(new User
+            {
+                Id = userId,
+            });
+            var passwordService = Substitute.For<IPasswordService>();
+            passwordService.CheckIfPasswordMatchesHash(Arg.Any<string>(), Arg.Any<HashedPassword>()).Returns(true);
+            var manager = new UserManager(adapter, passwordService);
+
+            // Act
+            var result = await manager.ChangePasswordAsync(userId, "oldpass", "newpass");
+
+            // Assert
+            Assert.AreEqual(ChangePasswordResults.PasswordChanged, result);
         }
     }
 }
