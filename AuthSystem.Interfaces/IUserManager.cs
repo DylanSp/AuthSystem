214eecf9270343f9c61e55140de041d052885cb6
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-07 18:55:55 -0400

    Make note on IUserManager.CreateUserAsync()

--- AuthSystem.Interfaces/IUserManager.cs
+++ AuthSystem.Interfaces/IUserManager.cs
@@ -19,6 +19,8 @@ namespace AuthSystem.Interfaces
     public interface IUserManager
     {
         Task<bool> ValidatePasswordAsync(string username, string password);
+
+        // TODO - return user ID when successful
         Task<CreateUserResults> CreateUserAsync(string username, string password);
         Task<ChangePasswordResults> ChangePasswordAsync(Guid userId, string oldPassword, string newPassword);
     }
