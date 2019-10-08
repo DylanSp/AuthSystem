1c967d017462efc2768d68810c6c64e5d3e2cf3a
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-08 07:12:31 -0400

    Add TODO note to IPermissionGrantManager

--- AuthSystem.Interfaces/IPermissionGrantManager.cs
+++ AuthSystem.Interfaces/IPermissionGrantManager.cs
@@ -12,6 +12,7 @@ namespace AuthSystem.Interfaces
 
     public interface IPermissionGrantManager
     {
+        // TODO - have these look up by userId or username?
         Task<Guid> CreatePermissionGrantAsync(Guid userId, Guid resourceId, PermissionType permission);    // TODO - return type? return differently if user/resource don't exist? do we need to return created permissionId?
         Task DeletePermissionGrantAsync(Guid permissionId);  // TODO - return type? return differently if permission doesn't exist?
         Task<bool> CheckIfUserHasPermissionAsync(Guid userId, Guid resourceId, PermissionType permission);   // TODO - return type? return differently if user/resource don't exist?
