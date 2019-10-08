731c443b4be5a360f9bc1def73e532cc9927e211
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-07 20:02:49 -0400

    Create IPermissionGrantAdapter, IPermissionGrantManager

---
+++ AuthSystem.Interfaces/IPermissionGrantAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Interfaces
+{
+    public interface IPermissionGrantAdapter
+    {
+        Task<Guid> CreatePermissionGrant(Guid userId, Guid resourceId, PermissionType permission);    // TODO - return type? return differently if user/resource don't exist?
+        Task DeletePermissionGrant(Guid permissionId);  // TODO - return type? return differently if permission doesn't exist?
+        Task<bool> CheckIfUserHasPermission(Guid userId, Guid resourceId, PermissionType permission);   // TODO - return type? return differently if user/resource don't exist?
+    }
+}
---
+++ AuthSystem.Interfaces/IPermissionGrantManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Interfaces
+{
+    public enum PermissionType
+    {
+        Unknown = 0,
+        Read = 1,
+        Write = 2,
+    }
+
+    public interface IPermissionGrantManager
+    {
+        Task<Guid> CreatePermissionGrant(Guid userId, Guid resourceId, PermissionType permission);    // TODO - return type? return differently if user/resource don't exist?
+        Task DeletePermissionGrant(Guid permissionId);  // TODO - return type? return differently if permission doesn't exist?
+        Task<bool> CheckIfUserHasPermission(Guid userId, Guid resourceId, PermissionType permission);   // TODO - return type? return differently if user/resource don't exist?
+    }
+}
