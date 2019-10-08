ed41997563065a51c36cbc5e9ebe3a12802b7673
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-07 20:19:10 -0400

    Add todo notes

---
+++ AuthSystem/TODO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthSystem
+{
+    // TODO - put Adapters, Managers folders in Interfaces project (make sure to add .Adapters/.Managers to namespaces)
+    // TODO - use ValueOf to create distinct UserId, PermissionGrantId, ResourceId types
+}
