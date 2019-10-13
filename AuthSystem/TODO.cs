c255dc8347b890a1cf26aaa1e1c4281595ffdf90
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-12 20:56:03 -0400

    Add TODO notes about CI

--- AuthSystem/TODO.cs
+++ AuthSystem/TODO.cs
@@ -7,4 +7,7 @@ namespace AuthSystem
 {
     // TODO - in UserManager, make (check for username uniqueness +  user creation) atomic to avoid race conditions?
     // TODO - implement adapters for resources, permission grants
+    // TODO - CI
+    // TODO - unit tests - dotnet test --filter TestCategory=UnitTest
+    // TODO - integration tests spin up Postgres container, run migrations + dotnet test --filter TestCategory=IntegrationTest ?
 }
