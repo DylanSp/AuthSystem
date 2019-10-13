0e3371cd90e17981cf32a3b625b67fc42c0ad4c2
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-12 20:25:15 -0400

    Start implementing PostgresUserAdapter, with tests

---
+++ AuthSystem.Adapters/PostgresUserAdapter.cs
@@ -0,0 +1,99 @@
+﻿using AuthSystem.Data;
+using AuthSystem.Interfaces.Adapters;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Adapters
+{
+    public class PostgresUserAdapter : IUserAdapter
+    {
+        private NpgsqlConnection Connection { get; }
+
+        public PostgresUserAdapter(NpgsqlConnection connection)
+        {
+            Connection = connection;
+        }
+
+        public async Task CreateAsync(User newUser)
+        {
+            var insertUserQuery = @"INSERT INTO Users (Id, Username, Base64PasswordHash, Base64Salt)
+                                    VALUES (@Id, @Username, @Hash, @Salt)";
+            using (var command = new NpgsqlCommand(insertUserQuery, Connection))
+            {
+                command.Parameters.AddWithValue("Id", newUser.Id.Value);
+                command.Parameters.AddWithValue("Username", newUser.Username.Value);
+                command.Parameters.AddWithValue("Hash", newUser.HashedPassword.Base64PasswordHash.Value);
+                command.Parameters.AddWithValue("Salt", newUser.HashedPassword.Base64Salt.Value);
+
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        public async Task<User?> GetUserByIdAsync(UserId userId)
+        {
+            var getUserQuery = @"SELECT Id, Username, Base64PasswordHash, Base64Salt
+                                 FROM Users
+                                 WHERE Id = @Id";
+            using (var command = new NpgsqlCommand(getUserQuery, Connection))
+            {
+                command.Parameters.AddWithValue("Id", userId.Value);
+                var reader = await command.ExecuteReaderAsync();
+                if (await reader.ReadAsync())
+                {
+                    var readId = (Guid) reader["Id"];
+                    var username = (string)reader["Username"];
+                    var hash = (string)reader["Base64PasswordHash"];
+                    var salt = (string)reader["Base64Salt"];
+                    return new User(UserId.From(readId), Username.From(username), new HashedPassword(Base64Hash.From(hash), Base64Salt.From(salt)));
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public async Task<User?> GetUserByUsernameAsync(Username username)
+        {
+            var getUserQuery = @"SELECT Id, Username, Base64PasswordHash, Base64Salt
+                                 FROM Users
+                                 WHERE Username = @Username";
+            using (var command = new NpgsqlCommand(getUserQuery, Connection))
+            {
+                command.Parameters.AddWithValue("Username", username.Value);
+                var reader = await command.ExecuteReaderAsync();
+                if (await reader.ReadAsync())
+                {
+                    var id = (Guid)reader["Id"];
+                    var readUsername = (string)reader["Username"];
+                    var hash = (string)reader["Base64PasswordHash"];
+                    var salt = (string)reader["Base64Salt"];
+                    return new User(UserId.From(id), Username.From(readUsername), new HashedPassword(Base64Hash.From(hash), Base64Salt.From(salt)));
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public async Task<bool> IsUsernameUniqueAsync(Username username)
+        {
+            var isUniqueQuery = "SELECT COUNT(*) FROM Users WHERE Username = @Username";
+            using (var command = new NpgsqlCommand(isUniqueQuery, Connection))
+            {
+                command.Parameters.AddWithValue("Username", username.Value);
+                var result = (long) await command.ExecuteScalarAsync();
+                return result <= 1;
+            }
+        }
+
+        public async Task<int> UpdateAsync(User newUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AuthSystem.Tests/Adapters/PostgresUserAdapterTests.cs
@@ -0,0 +1,101 @@
+﻿using AuthSystem.Adapters;
+using AuthSystem.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Npgsql;
+using System;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Tests.Adapters
+{
+    [TestClass]
+    public class PostgresUserAdapterTests
+    {
+        private NpgsqlConnection connection;
+
+        [TestInitialize]
+        public async Task Setup()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("TestConfig.json").Build();
+            var connectionString = config["connectionString"];
+            connection = new NpgsqlConnection(connectionString);
+            await connection.OpenAsync();
+        }
+
+        [TestCleanup]
+        public async Task Teardown()
+        {
+            await connection.DisposeAsync();
+        }
+
+        [TestMethod]
+        [TestCategory("IntegrationTest")]
+        public async Task Save_ThenReadById_RoundTrips()
+        {
+            // Arrange
+            var userToCreate = new User(UserId.From(Guid.NewGuid()), Username.From("Alfred"), new HashedPassword(Base64Hash.From("someHash"), Base64Salt.From("someSalt")));
+            var adapter = new PostgresUserAdapter(connection);
+
+            // Act
+            await adapter.CreateAsync(userToCreate);
+            var readUser = await adapter.GetUserByIdAsync(userToCreate.Id);
+
+            // Assert
+            Assert.IsNotNull(readUser);
+            Assert.AreEqual(userToCreate, readUser.Value);
+        }
+
+        [TestMethod]
+        [TestCategory("IntegrationTest")]
+        public async Task Save_ThenReadByName_RoundTrips()
+        {
+            // Arrange
+            var userToCreate = new User(UserId.From(Guid.NewGuid()), Username.From(Guid.NewGuid().ToString()), new HashedPassword(Base64Hash.From("someHash"), Base64Salt.From("someSalt")));
+            var adapter = new PostgresUserAdapter(connection);
+
+            // Act
+            await adapter.CreateAsync(userToCreate);
+            var readUser = await adapter.GetUserByUsernameAsync(userToCreate.Username);
+
+            // Assert
+            Assert.IsNotNull(readUser);
+            Assert.AreEqual(userToCreate, readUser.Value);
+        }
+
+        [TestMethod]
+        [TestCategory("IntegrationTest")]
+        public async Task Update_ThenRead_ReturnsUpdatedData()
+        {
+            // Arrange
+            var initialUser = new User(UserId.From(Guid.NewGuid()), Username.From(Guid.NewGuid().ToString()), new HashedPassword(Base64Hash.From("someHash"), Base64Salt.From("someSalt")));
+            var updatedUser = new User(initialUser.Id, initialUser.Username, new HashedPassword(initialUser.HashedPassword.Base64PasswordHash, Base64Salt.From("someOtherSalt")));
+
+            var adapter = new PostgresUserAdapter(connection);
+
+            // Act
+            await adapter.CreateAsync(initialUser);
+            await adapter.UpdateAsync(initialUser);
+            var readUser = await adapter.GetUserByIdAsync(updatedUser.Id);
+
+            // Assert
+            Assert.AreNotEqual(initialUser, readUser.Value);
+            Assert.AreEqual(updatedUser, readUser.Value);
+        }
+
+        [TestMethod]
+        [TestCategory("IntegrationTest")]
+        public async Task IsUsernameUnique_WithUniqueNameInDatabase_ReturnsTrue()
+        {
+            // Arrange
+            var user = new User(UserId.From(Guid.NewGuid()), Username.From(Guid.NewGuid().ToString()), new HashedPassword(Base64Hash.From("someHash"), Base64Salt.From("someSalt")));
+            var adapter = new PostgresUserAdapter(connection);
+
+            // Act
+            await adapter.CreateAsync(user);
+            var isUnique = await adapter.IsUsernameUniqueAsync(user.Username);
+
+            // Assert
+            Assert.IsTrue(isUnique);
+        }
+    }
+}
