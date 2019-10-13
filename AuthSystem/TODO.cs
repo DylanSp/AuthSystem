09897d3ea46d813878140ef3a110ece2ccd39f91
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-13 19:02:02 -0400

    Add more TODO notes

--- AuthSystem/TODO.cs
+++ AuthSystem/TODO.cs
@@ -9,6 +9,9 @@ namespace AuthSystem
     // TODO - in UserManager, make (check for username uniqueness +  user creation) atomic to avoid race conditions?
     // TODO - implement adapters for resources, permission grants
     // TODO - logging with Serilog
+    // TODO - add a permission for managing permissions on a resource (grant to resource creator, admin)
+    // TODO - need to autocreate admin user in DB in a migration (though how to handle password? default password that gets changed?)
+    // TODO - how to grant permission-changing permission to admin automatically? isAdmin column in the DB, look up all users with it set, give them perms?
     // TODO - CI
     // TODO - unit tests - dotnet test --filter TestCategory=UnitTest
     // TODO - integration tests spin up Postgres container, run migrations + dotnet test --filter TestCategory=IntegrationTest ?
