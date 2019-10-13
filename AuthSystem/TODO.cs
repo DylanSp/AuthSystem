ba9d0b2716d8cc38ca7fbb81781574272960ed39
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-12 21:15:21 -0400

    Add TODO about modifying ResourceManager

--- AuthSystem/TODO.cs
+++ AuthSystem/TODO.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace AuthSystem
 {
+    // TODO - make ResourceManager work with UserIds instead of Usernames
     // TODO - in UserManager, make (check for username uniqueness +  user creation) atomic to avoid race conditions?
     // TODO - implement adapters for resources, permission grants
     // TODO - CI
