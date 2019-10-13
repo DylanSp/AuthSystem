32764e2955bd1117f66191845b9c1170cde1ada3
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-12 20:32:51 -0400

    Remove unused SqlServerUserAdapter, remove finished TODOs

--- AuthSystem/TODO.cs
+++ AuthSystem/TODO.cs
@@ -6,7 +6,5 @@ using System.Threading.Tasks;
 namespace AuthSystem
 {
     // TODO - in UserManager, make (check for username uniqueness +  user creation) atomic to avoid race conditions?
-    // TODO - genericize adapters to just take IDbConnection?
-    // TODO - yank out Dapper from adapters, use ValueOf's From() when reading from DB
     // TODO - implement adapters for resources, permission grants
 }
