8ceed5e9848c11ef4361075b857c1a1281f475e5
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-13 12:08:16 -0400

    Add TODO about logging

--- AuthSystem/TODO.cs
+++ AuthSystem/TODO.cs
@@ -8,6 +8,7 @@ namespace AuthSystem
     // TODO - does ResourceManager need to worry about nonexistent user IDs? or is that handled by PermissionGrantManager?
     // TODO - in UserManager, make (check for username uniqueness +  user creation) atomic to avoid race conditions?
     // TODO - implement adapters for resources, permission grants
+    // TODO - logging with Serilog
     // TODO - CI
     // TODO - unit tests - dotnet test --filter TestCategory=UnitTest
     // TODO - integration tests spin up Postgres container, run migrations + dotnet test --filter TestCategory=IntegrationTest ?
