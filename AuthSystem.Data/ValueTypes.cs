9f904759a83be99d3f088a77eb268cae2b7b0e8a
Dylan Sprague <dsprague@docuphase.com>
2019-10-15 12:44:47 -0400

    Implement PasswordService, with tests

--- AuthSystem.Data/ValueTypes.cs
+++ AuthSystem.Data/ValueTypes.cs
@@ -45,5 +45,17 @@ namespace AuthSystem.Data
     public class Base64Salt : ValueOf<string, Base64Salt>
     {
     }
+
+    public class IterationCount : ValueOf<int, IterationCount>
+    {
+    }
+
+    public class SaltLength : ValueOf<int, SaltLength>
+    {
+    }
+
+    public class KeyLength : ValueOf<int, KeyLength>
+    {
+    }
 }
 
