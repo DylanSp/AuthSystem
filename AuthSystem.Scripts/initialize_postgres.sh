78ca8f8688c4c7ccefcd7554e820c713ef2309d3
Dylan Sprague <dsprague@docuphase.com>
2019-10-17 16:10:26 -0400

    Add sudo to pipeline script commands

--- AuthSystem.Scripts/initialize_postgres.sh
+++ AuthSystem.Scripts/initialize_postgres.sh
@@ -2,16 +2,16 @@
 
 docker run --name auth-postgres -e POSTGRES_PASSWORD=adminpass -e POSTGRES_DB=AuthSystem -d -p 5432:5432 postgres:12
 
-apt-get update -y
+sudo apt-get update -y
 
-apt install curl -y
+sudo apt-get install curl -y
 
-cd ~ || exit
+cd / || exit
 
 # prequisite for evolve
-apt install libicu60 -y
+sudo apt-get install libicu60 -y
 
-curl -L -o evolve.tar.gz https://github.com/lecaillon/Evolve/releases/download/2.3.0/evolve_2.3.0_Linux-64bit.tar.gz
-tar -zxvf evolve.tar.gz
+sudo curl -L -o evolve.tar.gz https://github.com/lecaillon/Evolve/releases/download/2.3.0/evolve_2.3.0_Linux-64bit.tar.gz
+sudo tar -zxvf evolve.tar.gz
 
-evolve migrate postgresql -c "Server=127.0.0.1;Database=AuthSystem;User Id=postgres;Password=adminpass;" -l "AuthSystem.Scripts/migrations" -s public --metadata-table Evolve_Changelog
+sudo ./evolve migrate postgresql -c "Server=127.0.0.1;Database=AuthSystem;User Id=postgres;Password=adminpass;" -l "AuthSystem.Scripts/migrations" -s public --metadata-table Evolve_Changelog
