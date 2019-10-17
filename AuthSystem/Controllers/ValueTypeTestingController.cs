9191fcd86a3046fff1ab80df5dd8cf782a4459e5
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-17 05:31:31 -0400

    Add tests of how deserialization fails for structs/ValueOf types in controllers

--- AuthSystem/Controllers/ValueTypeTestingController.cs
+++ AuthSystem/Controllers/ValueTypeTestingController.cs
@@ -8,6 +8,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AuthSystem.Controllers
 {
+    public struct TestStruct
+    {
+        public int TestMember { get; }
+        
+        public TestStruct(int testMember)
+        {
+            TestMember = testMember;
+        }
+    }
+
+    public struct TestStructWithSetter
+    {
+        public int TestMember { get; set; }
+    }
+
     [Route("api/valuetesting")]
     [ApiController]
     public class ValueTypeTestingController : ControllerBase
@@ -23,6 +38,33 @@ namespace AuthSystem.Controllers
         [Route("users/{userId:guid}")]
         public ActionResult<string> TestUserId([FromRoute] UserId userId)
         {
+            return "ok";
+        }
+
+        [HttpPost]
+        [Route("simpleSetter")]
+        public ActionResult<string> TestSimpleDeserializationThatShouldWork([FromBody] TestStructWithSetter testStruct)
+        {
+            // given { "TestMember": 3 }
+            // testStruct.TestMember should equal 3, actually is 3
+            return "ok";
+        }
+
+        [HttpPost]
+        [Route("simple")]
+        public ActionResult<string> TestSimpleDeserialization([FromBody] TestStruct testStruct)
+        {
+            // given { "TestMember": 3 }
+            // testStruct.TestMember should equal 3, actually is 0
+            return "ok";
+        }
+
+        [HttpPost]
+        [Route("hashes")]
+        public ActionResult<string> TestDeserialization([FromBody] HashedPassword hashedPassword)
+        {
+            // given { "Base64PasswordHash": "hash", "Base64Salt": "salt" }
+            // should set values, actually gives validation error
             return "ok";
         }
     }
