a04a6466af23778e85bf197cfe375c73eb21cbbb
Dylan Sprague <dsprague@docuphase.com>
2019-10-17 16:17:27 -0400

    Make changes to pipeline script for Azure agent

--- AuthSystem.Scripts/initialize_postgres.sh
+++ AuthSystem.Scripts/initialize_postgres.sh
@@ -4,14 +4,14 @@ docker run --name auth-postgres -e POSTGRES_PASSWORD=adminpass -e POSTGRES_DB=Au
 
 sudo apt-get update -y
 
-sudo apt-get install curl -y
-
 cd / || exit
 
 # prequisite for evolve
-sudo apt-get install libicu60 -y
+sudo apt-get install libicu55 -y
 
 sudo curl -L -o evolve.tar.gz https://github.com/lecaillon/Evolve/releases/download/2.3.0/evolve_2.3.0_Linux-64bit.tar.gz
 sudo tar -zxvf evolve.tar.gz
 
-sudo ./evolve migrate postgresql -c "Server=127.0.0.1;Database=AuthSystem;User Id=postgres;Password=adminpass;" -l "AuthSystem.Scripts/migrations" -s public --metadata-table Evolve_Changelog
+cd "$OLDPWD" || exit # return to source root
+
+sudo /evolve migrate postgresql -c "Server=127.0.0.1;Database=AuthSystem;User Id=postgres;Password=adminpass;" -l "AuthSystem.Scripts/migrations" -s public --metadata-table Evolve_Changelog
