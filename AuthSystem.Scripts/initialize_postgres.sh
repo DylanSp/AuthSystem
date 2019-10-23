a19800c3e375b91cb968a0f9085ba8695df609af
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-23 01:01:29 -0400

    Clean up initialize_postgres.sh CI script

--- AuthSystem.Scripts/initialize_postgres.sh
+++ AuthSystem.Scripts/initialize_postgres.sh
@@ -1,17 +1,10 @@
-﻿#!/bin/bash
+﻿#!/usr/bin/env bash
 
 docker run --name auth-postgres -e POSTGRES_PASSWORD=adminpass -e POSTGRES_DB=AuthSystem -d -p 5432:5432 postgres:12
 
 sudo apt-get update -y
 
-cd / || exit
-
-# prequisite for evolve
-sudo apt-get install libicu55 -y
-
 sudo curl -L -o evolve.tar.gz https://github.com/lecaillon/Evolve/releases/download/2.3.0/evolve_2.3.0_Linux-64bit.tar.gz
 sudo tar -zxvf evolve.tar.gz
 
-cd "$OLDPWD" || exit # return to source root
-
-sudo /evolve migrate postgresql -c "Server=127.0.0.1;Database=AuthSystem;User Id=postgres;Password=adminpass;" -l "AuthSystem.Scripts/migrations" -s public --metadata-table Evolve_Changelog
+sudo ./evolve migrate postgresql -c "Server=127.0.0.1;Database=AuthSystem;User Id=postgres;Password=adminpass;" -l "AuthSystem.Scripts/migrations" -s public --metadata-table Evolve_Changelog
