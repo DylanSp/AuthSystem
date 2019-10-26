7d24129725e1cde9c37d1dfe5b8dc9800c955705
Dylan Sprague <dylan.richard.sprague@gmail.com>
2019-10-26 00:48:59 -0400

    Add validation requirements to UserAuthenticationDTO

--- AuthSystem/DTOs/UserAuthenticationDTO.cs
+++ AuthSystem/DTOs/UserAuthenticationDTO.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AuthSystem.DTOs
 {
     public class UserAuthenticationDTO
     {
+        [Required]
+        [MinLength(1, ErrorMessage = "Username must be at least 1 character")]
         public string Username { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Password must be at least 1 character")]
         public string Password { get; set; }
     }
 }
