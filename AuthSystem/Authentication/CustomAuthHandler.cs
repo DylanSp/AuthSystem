2ce344dc04478ee84f96bdd558730ee2298911e3
Dylan Sprague <dsprague@docuphase.com>
2019-10-29 16:49:04 -0400

    Begin implementing custom authentication, AuthenticationController

--- AuthSystem/Authentication/CustomAuthHandler.cs
+++ AuthSystem/Authentication/CustomAuthHandler.cs
@@ -5,20 +5,38 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using AuthSystem.Data;
+using AuthSystem.Interfaces;
 
 namespace AuthSystem.Authentication
 {
     internal class CustomAuthHandler : AuthenticationHandler<CustomAuthOptions>
     {
+        private IJwtService JwtService { get; }
+
         public CustomAuthHandler(IOptionsMonitor<CustomAuthOptions> options, ILoggerFactory logger, UrlEncoder encoder,
-            ISystemClock clock) : base(options, logger, encoder, clock)
+            ISystemClock clock, IJwtService jwtService) : base(options, logger, encoder, clock)
         {
-            // store custom services here...
+            // store custom services here... (request in ctor)
+            JwtService = jwtService;
         }
 
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
-            return AuthenticateResult.Success(new AuthenticationTicket(new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>(), Scheme.Name)),new AuthenticationProperties(), Scheme.Name));
+            string jwt;
+            if (!Request.Cookies.TryGetValue("AccessToken", out jwt))
+            {
+                return AuthenticateResult.Fail("No AccessToken cookie attached");
+            }
+
+            var userId = JwtService.DecodeToken(new JsonWebToken(jwt));
+            if (!userId.HasValue)
+            {
+                return AuthenticateResult.Fail("AccessToken invalid");
+            }
+
+            var claim = new Claim("UserId", userId.Value.Value.ToString());
+            return AuthenticateResult.Success(new AuthenticationTicket(new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { claim }, Scheme.Name)),new AuthenticationProperties(), Scheme.Name));
             
             // build the claims and put them in "Context"; you need to import the Microsoft.AspNetCore.Authentication package
             // return AuthenticateResult.NoResult();
--- AuthSystem/Controllers/AuthenticationController.cs
+++ AuthSystem/Controllers/AuthenticationController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
+using AuthSystem.Data;
+using AuthSystem.Interfaces.Managers;
 
 namespace AuthSystem.Controllers
 {
@@ -10,11 +12,30 @@ namespace AuthSystem.Controllers
     [Route("v{version:apiVersion}")]
     public class AuthenticationController : ControllerBase
     {
+        private IUserManager UserManager { get; }
+
+        public AuthenticationController(IUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
         [HttpPost]
         [Route("login")]
         public async Task<IActionResult> LoginAsync([FromBody] UserAuthenticationDTO userAuthentication)
         {
-            throw new NotImplementedException();
+            var isCorrectPassword = await UserManager.ValidatePasswordAsync(new Username(userAuthentication.Username),
+                new PlaintextPassword(userAuthentication.Password));
+
+            if (isCorrectPassword)
+            {
+                // TODO - construct access token, refresh token, save refresh token, return tokens
+                // TODO - access token should have user ID, not username
+                return Ok();
+            }
+            else
+            {
+                return Unauthorized();
+            }
         }
 
         [HttpPost]
