c5d775d84ad2a06eb11ccb73c8e99cd99e27487b
Dylan Sprague <dsprague@docuphase.com>
2020-01-21 13:14:55 -0500

    Begin writing web integration tests for authentication

---
+++ AuthSystem.WebTests/AuthenticationTests.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AuthSystem.DTOs;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AuthSystem.WebTests
+{
+    [TestClass]
+    public class AuthenticationTests
+    {
+        private static WebApplicationFactory<Startup> _factory;
+        private static string _username;
+        private static string _password;
+
+        [ClassInitialize]   // run once before all tests
+        public static async Task Setup(TestContext context)
+        {
+            _factory = new WebApplicationFactory<Startup>();
+
+            // initialize user
+            _username = Guid.NewGuid().ToString();
+            _password = Guid.NewGuid().ToString();
+
+            var client = _factory.CreateClient();
+            var userCreationResponse = await client.PostAsJsonAsync("/v1/users", new UserAuthenticationDTO
+            {
+                Username = _username,
+                Password = _password,
+            });
+
+            if (!userCreationResponse.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to initialize user for Web {nameof(AuthenticationTests)}");
+            }
+        }
+
+        [TestMethod]
+        public async Task Login_WithValidCredentials_ReturnsOk()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act 
+            var response = await client.PostAsJsonAsync("/v1/login", new UserAuthenticationDTO
+            {
+                Username = _username,
+                Password = _password,
+            });
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task Login_WithInvalidCredentials_ReturnsUnauthorized()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsJsonAsync("/v1/login", new UserAuthenticationDTO
+            {
+                Username = Guid.NewGuid().ToString(),
+                Password = Guid.NewGuid().ToString(),
+            });
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task Login_WithValidCredentials_SetsSessionCookie()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsJsonAsync("/v1/login", new UserAuthenticationDTO
+            {
+                Username = _username,
+                Password = _password,
+            });
+
+            // Assert
+            Assert.IsTrue(response.Headers.Contains("Set-Cookie"));
+            Assert.IsTrue(response.Headers.GetValues("Set-Cookie")
+                .Any(headerValue => headerValue.Contains(Constants.SESSION_COOKIE_NAME)));
+        }
+    }
+}
