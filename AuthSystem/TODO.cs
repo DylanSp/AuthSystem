c6fb8f67682e02837387bc5b74aff26b3c03c693
Dylan Sprague <dsprague@docuphase.com>
2020-01-27 15:19:00 -0500

    Update TODO list

--- AuthSystem/TODO.cs
+++ AuthSystem/TODO.cs
@@ -10,7 +10,6 @@ namespace AuthSystem
     // TODO - grant permission mgmt permission to admin
     // TODO - need to autocreate admin user in DB in a migration (though how to handle password? default password that gets changed?)
     // TODO - how to grant permission-changing permission to admin automatically? isAdmin column in the DB, look up all users with it set, give them perms?
-    // TODO - integration tests (separate categories for DB integration tests and fullstack integration tests; probably separate project for the latter)
     // TODO - integration test(s) for CustomAuthHandler
     // TODO - UserController/AuthenticationController integration test - create user than login with same password succeeds
     // TODO - AuthenticationController integration test - logout, then try to access protected resource fails
